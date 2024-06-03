9d686eedbdd7ce18d812c3ea290673f0d58a2778
Tango797 <tewatia35@gmail.com>
2024-06-03 12:31:58 +0530

    9_jwt and user and video model

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'ravitewatiaweb', 
+  api_key: '876688274535678', 
+  api_secret: 'NkfWfdzTj9uDIbV618ujZHmxmOM' 
+});
